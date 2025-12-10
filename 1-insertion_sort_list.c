8ca0de3c93e976fb3bd2df65f6cd4003702065c4
Antoine <antoine.bats@gmail.com>
2025-12-10 10:22:18 +0000

    Ex1 suppresion fonction helper

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,56 +1,5 @@
 #include "sort.h"
 /**
-* find_position - Finds the correct insertion position
-*					for a value in sorted list.
-* @head: Pointer to the head of the sorted list.
-* @value: Value to insert.
-*
-* Return: Pointer to first node >= value, or NULL.
-*/
-listint_t *find_position(listint_t *head, int value)
-{
-	while (head != NULL && head->n < value)
-		head = head->next;
-
-	return (head);
-}
-
-/**
-* move_node - Moves a node before a specified position in a doubly linked list.
-* @current: Pointer to the node to move.
-* @new_position: Pointer to the node before which @current must be inserted.
-* @list: Double pointer to the head of the list.
-*/
-void move_node(listint_t *current, listint_t *new_position, listint_t **list)
-{
-	listint_t *prev = current->prev;
-	listint_t *next = current->next;
-
-	if (prev)
-		prev->next = next;
-	else
-		*list = next;
-
-	if (next)
-		next->prev = prev;
-
-	if (new_position == *list)
-	{
-		current->prev = NULL;
-		current->next = *list;
-		(*list)->prev = current;
-		*list = current;
-		return;
-	}
-
-	current->next = new_position;
-	current->prev = new_position->prev;
-
-	new_position->prev->next = current;
-	new_position->prev = current;
-}
-
-/**
 * insertion_sort_list - Sorts a doubly linked list using insertion sort.
 * @list: Double pointer to the head of the list.
 */
@@ -65,29 +14,28 @@ void insertion_sort_list(listint_t **list)
 
 	while (current)
 	{
-		temp = current->next;
+		temp = current;
+		current = current->next;
 
-		while (current->prev != NULL && current->n < current->prev->n)
+		while (temp->prev != NULL && temp->n < temp->prev->n)
 		{
-			prev_n = current->prev;
+			prev_n = temp->prev;
 
-			prev_n->next = current->next;
-			if (current->next != NULL)
-				current->next->prev = prev_n;
+			prev_n->next = temp->next;
+			if (temp->next != NULL)
+				temp->next->prev = prev_n;
 
-			current->prev = prev_n->prev;
-			current->next = prev_n;
+			temp->next = prev_n;
+			temp->prev = prev_n->prev;
 
 			if (prev_n->prev != NULL)
-				prev_n->prev->next = current;
+				prev_n->prev->next = temp;
 			else
-				*list = current;
+				*list = temp;
 
-			prev_n->prev = current;
+			prev_n->prev = temp;
 
 			print_list(*list);
 		}
-
-		current = temp;
 	}
 }
